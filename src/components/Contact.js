8b2aff613159f60c29df3121c32f6785db595b2c
lazyvamp <lazyvamp16@gmail.com>
2024-05-22 22:10:38 +0530

    updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,10 +8,10 @@ const Contact = () => {
       <form id='form' className={`${styles.form} text-left`}>
         <h2>Contact us</h2>
         <p>I'm the monkey man!</p>
-        <MDBInput label='Name' id='name' wrapperClass='mb-4' />
-        <MDBInput type='email' label='Email address' id='email' wrapperClass='mb-4' />
-        <MDBInput label='Subject' id='subject' wrapperClass='mb-4' />
-        <MDBTextArea wrapperClass='mb-4' label='Message' id='message' />
+        <MDBInput placeholder='Name' id='name' wrapperClass='mb-4' />
+        <MDBInput type='email' placeholder='Email address' id='email' wrapperClass='mb-4' />
+        <MDBInput placeholder='Subject' id='subject' wrapperClass='mb-4' />
+        <MDBTextArea wrapperClass='mb-4' placeholder='Message' id='message' />
         <MDBCheckbox wrapperClass='d-flex justify-content-left' label='Send me a copy' id='sendCopy' />
         <MDBBtn color='primary' block className='my-4'>
           Send
